6f9ad95c130f6520a6b965b621dad73185160841
Egoru Froggy <Impulsivethinker@yadex.ru>
2023-02-02 17:13:07 +0300

    updated brain_prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -4,17 +4,15 @@ from random import randint
 start_word = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def prime_check(number):
-    result = 'yes'
-    for i in range(2, number // 2 + 1):
-        if number % i == 0:
-            result = 'no'
-        i += 1
-    return result
-
-
 def play():
-    number = randint(3, 100)
-    result = prime_check(number)
-    question = f'Question: {number}'
+    num1 = randint(1, 100)
+    question = num1
+    k = 0
+    for i in range(2, num1 // 2 + 1):
+        if (num1 % i == 0):
+            k += 1
+    if (k <= 0):
+        result = "yes"
+    else:
+        result = 'no'
     return question, result
